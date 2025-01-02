3a0e04b15262e7267900a8782b82fc2dc1f6398e
RobertHendry <roberthendrygames@gmail.com>
2025-01-02 19:54:33 +0000

    5h35m Upload
    Fixed water to be 2 sided

--- RH_Kratosmas/Assets/Scripts/CameraController.cs
+++ RH_Kratosmas/Assets/Scripts/CameraController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class CameraController : MonoBehaviour
 {
     public Transform seagullTransform;
     private Vector3 cameraOffset;
     private float followDistance = 5f;
+    public Volume volume;
+    private ColorAdjustments colorAdjustments;
+    private Color underwaterColor = new Color(2f / 255f, 90f / 255f, 135f / 255f);// (0f, 101f / 255f, 178f / 255f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +24,28 @@ public class CameraController : MonoBehaviour
     {
         transform.position = seagullTransform.position - seagullTransform.forward * followDistance;
         transform.LookAt(seagullTransform);
+
+        if(transform.position.y < 5)
+        {
+            if (volume != null)
+            {
+                volume.profile.TryGet(out ColorAdjustments colorAdjustments);
+                if (colorAdjustments != null)
+                {
+                    colorAdjustments.colorFilter.value = underwaterColor;
+                }
+            }
+        }
+        else
+        {
+            if (volume != null)
+            {
+                volume.profile.TryGet(out ColorAdjustments colorAdjustments);
+                if (colorAdjustments != null)
+                {
+                    colorAdjustments.colorFilter.value = Color.white;
+                }
+            }
+        }
     }
 }
